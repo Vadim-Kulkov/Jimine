bc3aaf6af9c4b11ea731d623c7a810a6577e77c5
Vadim-Kulkov <manavar2017@yandex.com>
2023-11-06 17:57:05 +0400

    [6.11.2023 - Backend] Initial commit

---
+++ JimineBackend/src/main/java/com/jimine/jiminebackend/JimineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jimine.jiminebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimineBackendApplication.class, args);
+    }
+
+}
---
+++ JimineBackend/src/test/java/com/jimine/jiminebackend/JimineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimine.jiminebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
